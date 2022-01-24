fa2a91016d4db3fe5381418dca1e10748cf26ae5
FelipeRLeon <feliiperestrepoleon@gmail.com>
2022-01-24 00:41:24 -0500

    Navegacion Especialiadad Completa

--- app/src/main/java/com/bbt/proyectocoaspharma/Principal/especialidadfragments/EspecialidadesInfografiaFragment.java
+++ app/src/main/java/com/bbt/proyectocoaspharma/Principal/especialidadfragments/EspecialidadesInfografiaFragment.java
@@ -2,59 +2,23 @@ package com.bbt.proyectocoaspharma.Principal.especialidadfragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.bbt.proyectocoaspharma.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EspecialidadesInfografiaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EspecialidadesInfografiaFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public EspecialidadesInfografiaFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EspecialidadesInfografiaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EspecialidadesInfografiaFragment newInstance(String param1, String param2) {
-        EspecialidadesInfografiaFragment fragment = new EspecialidadesInfografiaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +26,38 @@ public class EspecialidadesInfografiaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_especialidades_infografia, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btn_next = view.findViewById(R.id.btn_next);
+        Button btn_back = view.findViewById(R.id.btn_back);
+        Button btn_cerrar = view.findViewById(R.id.btn_cerrar);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.popBackStack(R.id.especialidadesVideoFragment, true);
+                navController.navigate(R.id.especialidadesVideoFragment);
+            }
+        });
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.popBackStack(R.id.especialidadesPresentacionesFragment, true);
+                navController.navigate(R.id.especialidadesPresentacionesFragment);
+            }
+        });
+        btn_cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.popBackStack(R.id.especialidadesMenuFragment, true);
+                navController.navigate(R.id.especialidadesMenuFragment);
+            }
+        });
     }
 }
--- app/src/main/java/com/bbt/proyectocoaspharma/Principal/especialidadfragments/EspecialidadesPresentacionesFragment.java
+++ app/src/main/java/com/bbt/proyectocoaspharma/Principal/especialidadfragments/EspecialidadesPresentacionesFragment.java
@@ -2,59 +2,23 @@ package com.bbt.proyectocoaspharma.Principal.especialidadfragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.bbt.proyectocoaspharma.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EspecialidadesPresentacionesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EspecialidadesPresentacionesFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public EspecialidadesPresentacionesFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EspecialidadesPresentacionesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EspecialidadesPresentacionesFragment newInstance(String param1, String param2) {
-        EspecialidadesPresentacionesFragment fragment = new EspecialidadesPresentacionesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +26,38 @@ public class EspecialidadesPresentacionesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_especialidades_presentaciones, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btn_next = view.findViewById(R.id.btn_next);
+        Button btn_back = view.findViewById(R.id.btn_back);
+        Button btn_cerrar = view.findViewById(R.id.btn_cerrar);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.popBackStack(R.id.especialidadesInfografiaFragment, true);
+                navController.navigate(R.id.especialidadesInfografiaFragment);
+            }
+        });
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.popBackStack(R.id.especialidadesVideoFragment, true);
+                navController.navigate(R.id.especialidadesVideoFragment);
+            }
+        });
+        btn_cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.popBackStack(R.id.especialidadesMenuFragment, true);
+                navController.navigate(R.id.especialidadesMenuFragment);
+            }
+        });
     }
 }
